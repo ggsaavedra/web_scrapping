3c1d5976aeec60f170ed2708634c5d2f4721199a
Maggie <maggie.saavedra@neuralmechanics.net>
2018-04-25 15:00:34 +0800

    some changes in blooberg scrapper

--- bloomberg/bloom_scrape.R
+++ bloomberg/bloom_scrape.R
@@ -1,26 +1,25 @@
 
 
 library(rvest)
-url <- 'https://www.bloomberg.com/search?query=philippines&endTime=2018-04-24T03:18:40.983Z&page='
+library(data.table)
+link <- 'https://www.bloomberg.com/search?query=philippines&endTime=2018-04-25T03:18:40.983Z&page='
 
-i=0
-repeat{
-  i = i+1
-  download.file(paste0(url,i), 'tmp1')
-  url <- read_html('tmp1')
-  date <- html_text(html_nodes(url, '.published-at'))
+for (i in 50:1363){
+  url <- read_html(paste0(link,i))
+  date <- as.POSIXct(strptime(html_text(html_nodes(url, '.published-at')), format = ' %b %d, %Y '))
   headline <- html_text(html_nodes(url, '.search-result-story__headline a'))
-  
-  content <- NULL
-  href <- html_attrs(html_nodes(url, '.search-result-story__headline a'))
-  for (j in 1:length(href)){
-    download.file(href[[j]]['href'], 'tmp2')
-    body_url <- read_html('tmp2')
-    content[j] <- html_text(html_nodes(body_url, '.fence-body'))
-    Sys.sleep(5)
-  }
-
-  
+  df <- data.table(date,headline)
+  fwrite(df, 'bloomberg_articles.csv', append = TRUE, row.names = FALSE)
+  # content <- NULL
+  # href <- html_attrs(html_nodes(url, '.search-result-story__headline a'))
+  # for (j in 1:length(href)){
+  #   download.file(href[[j]]['href'], 'tmp2')
+  #   body_url <- read_html('tmp2')
+  #   content[j] <- html_text(html_nodes(body_url, '.fence-body'))
+  #   Sys.sleep(sample(10:100, 1))
+  # }
+  Sys.sleep(sample(5:10, 1))
+  print(paste('Done with page', i))
 }
 
 
